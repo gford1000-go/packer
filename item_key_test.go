fa41a4c3004b755f7e999310e775459781bdbe86
Geoff Ford <geoff.golang.12@gmail.com>
2025-03-02 09:37:43 +0000

    Added further tests for EnvelopeKeyProvider

--- item_key_test.go
+++ item_key_test.go
@@ -8,6 +8,8 @@ import (
 	"errors"
 	"math/big"
 	"testing"
+
+	"github.com/gford1000-go/serialise"
 )
 
 func TestNewEnvelopeKeyProvider(t *testing.T) {
@@ -133,5 +135,248 @@ func TestNewEnvelopeKeyProvider_1(t *testing.T) {
 			t.Fatal("Created duplicate keys")
 		}
 		keys[string(key)] = true
+	}
+}
+
+func TestNewEnvelopeKeyProvider_2(t *testing.T) {
+	p, err := NewEnvelopeKeyProvider(nil, nil)
+	if err == nil {
+		t.Fatal("Unexpected success when expected error")
+	}
+	if !errors.Is(err, ErrMissingEnvelopeKeyProviderInfo) {
+		t.Fatalf("Unexpected error: expected: %v, got: %v", ErrMissingEnvelopeKeyProviderInfo, err)
+	}
+	if p != nil {
+		t.Fatal("Expected nil provider, but got instance")
+	}
+}
+
+func TestNewEnvelopeKeyProvider_3(t *testing.T) {
+	ki := &EnvelopeKeyProviderInfo{}
+	p, err := NewEnvelopeKeyProvider(ki, nil)
+	if err == nil {
+		t.Fatal("Unexpected success when expected error")
+	}
+	if !errors.Is(err, ErrProviderMustHaveAnID) {
+		t.Fatalf("Unexpected error: expected: %v, got: %v", ErrProviderMustHaveAnID, err)
+	}
+	if p != nil {
+		t.Fatal("Expected nil provider, but got instance")
+	}
+}
+
+func TestNewEnvelopeKeyProvider_4(t *testing.T) {
+	ki := &EnvelopeKeyProviderInfo{
+		ID: "anID",
+	}
+	p, err := NewEnvelopeKeyProvider(ki, nil)
+	if err == nil {
+		t.Fatal("Unexpected success when expected error")
+	}
+	if !errors.Is(err, ErrProviderMustHaveKey) {
+		t.Fatalf("Unexpected error: expected: %v, got: %v", ErrProviderMustHaveKey, err)
+	}
+	if p != nil {
+		t.Fatal("Expected nil provider, but got instance")
+	}
+}
+
+func TestNewEnvelopeKeyProvider_5(t *testing.T) {
+	ki := &EnvelopeKeyProviderInfo{
+		ID:  "anID",
+		Key: []byte("01234567890123456789012345678901"),
+	}
+	p, err := NewEnvelopeKeyProvider(ki, nil)
+	if err == nil {
+		t.Fatal("Unexpected success when expected error")
+	}
+	if !errors.Is(err, ErrMissingFinder) {
+		t.Fatalf("Unexpected error: expected: %v, got: %v", ErrMissingFinder, err)
+	}
+	if p != nil {
+		t.Fatal("Expected nil provider, but got instance")
+	}
+}
+
+func TestNewEnvelopeKeyProvider_6(t *testing.T) {
+	ki := &EnvelopeKeyProviderInfo{
+		ID:  "anID",
+		Key: []byte("01234567890123456789012345678901"),
+	}
+
+	finder := func(EnvelopeKeyID) (EnvelopeKeyProvider, error) {
+		return nil, errors.New("unknown ID")
+	}
+
+	p, err := NewEnvelopeKeyProvider(ki, finder)
+	if err != nil {
+		t.Fatalf("Unexpected failure when expected success: %v", err)
+	}
+	if p == nil {
+		t.Fatal("Expected provider instance, but got nil")
+	}
+}
+
+func TestNewEnvelopeKeyProvider_Decrypt(t *testing.T) {
+	ki := &EnvelopeKeyProviderInfo{
+		ID:  "anID",
+		Key: []byte("01234567890123456789012345678901"),
+	}
+
+	finder := func(EnvelopeKeyID) (EnvelopeKeyProvider, error) {
+		return nil, errors.New("unknown ID")
+	}
+
+	p, err := NewEnvelopeKeyProvider(ki, finder)
+	if err != nil {
+		t.Fatalf("Unexpected failure when expected success: %v", err)
+	}
+
+	b, err := p.Decrypt(context.TODO(), nil)
+	if err == nil {
+		t.Fatal("Unexpected success when expected error")
+	}
+	if b != nil {
+		t.Fatal("Unexpected instance returned when expected nil")
+	}
+}
+
+func TestNewEnvelopeKeyProvider_Decrypt_1(t *testing.T) {
+	ki := &EnvelopeKeyProviderInfo{
+		ID:  "anID",
+		Key: []byte("01234567890123456789012345678901"),
+	}
+
+	finder := func(EnvelopeKeyID) (EnvelopeKeyProvider, error) {
+		return nil, errors.New("unknown ID")
+	}
+
+	p, err := NewEnvelopeKeyProvider(ki, finder)
+	if err != nil {
+		t.Fatalf("Unexpected failure when expected success: %v", err)
+	}
+
+	b, err := p.Decrypt(context.TODO(), []byte{})
+	if err == nil {
+		t.Fatal("Unexpected success when expected error")
+	}
+	if b != nil {
+		t.Fatal("Unexpected instance returned when expected nil")
+	}
+}
+
+func TestNewEnvelopeKeyProvider_Decrypt_2(t *testing.T) {
+	ki := &EnvelopeKeyProviderInfo{
+		ID:  "anID",
+		Key: []byte("01234567890123456789012345678901"),
+	}
+
+	finder := func(EnvelopeKeyID) (EnvelopeKeyProvider, error) {
+		return nil, errors.New("unknown ID")
+	}
+
+	p, err := NewEnvelopeKeyProvider(ki, finder)
+	if err != nil {
+		t.Fatalf("Unexpected failure when expected success: %v", err)
+	}
+
+	// Pass invalid []byte to Decrypt
+	b, _, err := serialise.ToBytes(string("oops"), serialise.WithSerialisationApproach(serialise.NewMinDataApproachWithVersion(serialise.V1)))
+	if err != nil {
+		t.Fatalf("Unexpected failure when expected success: %v", err)
+	}
+
+	b, err = p.Decrypt(context.TODO(), b)
+	if err == nil {
+		t.Fatal("Unexpected success when expected error")
+	}
+	if b != nil {
+		t.Fatal("Unexpected instance returned when expected nil")
+	}
+}
+
+func TestNewEnvelopeKeyProvider_Decrypt_3(t *testing.T) {
+	ki := &EnvelopeKeyProviderInfo{
+		ID:  "anID",
+		Key: []byte("01234567890123456789012345678901"),
+	}
+
+	errID := errors.New("unknown ID")
+
+	finder := func(EnvelopeKeyID) (EnvelopeKeyProvider, error) {
+		return nil, errID
+	}
+
+	p, err := NewEnvelopeKeyProvider(ki, finder)
+	if err != nil {
+		t.Fatalf("Unexpected failure when expected success: %v", err)
+	}
+
+	ep, ok := p.(*evKeyProvider)
+	if !ok {
+		t.Fatalf("Unexpected cast error: %T", p)
+	}
+	b, err := ep.enc([]byte("bad key"))
+	if err != nil {
+		t.Fatalf("Unexpected failure when expected success: %v", err)
+	}
+
+	// Hand-crafted encrypted key fails to deserialise
+	b, err = p.Decrypt(context.TODO(), b)
+	if err == nil {
+		t.Fatal("Unexpected success when expected error")
+	}
+	if !errors.Is(err, serialise.ErrMinDataTypeNotDeserialisable) {
+		t.Fatalf("Unexpected error: expected: %v, got: %v", serialise.ErrMinDataTypeNotDeserialisable, err)
+	}
+	if b != nil {
+		t.Fatal("Unexpected instance returned when expected nil")
+	}
+}
+
+func TestNewEnvelopeKeyProvider_Decrypt_4(t *testing.T) {
+	ki := &EnvelopeKeyProviderInfo{
+		ID:  "anID",
+		Key: []byte("01234567890123456789012345678901"),
+	}
+
+	errID := errors.New("unknown ID")
+
+	finder := func(EnvelopeKeyID) (EnvelopeKeyProvider, error) {
+		return nil, errID
+	}
+
+	p, err := NewEnvelopeKeyProvider(ki, finder)
+	if err != nil {
+		t.Fatalf("Unexpected failure when expected success: %v", err)
+	}
+
+	ep, ok := p.(*evKeyProvider)
+	if !ok {
+		t.Fatalf("Unexpected cast error: %T", p)
+	}
+	b, err := ep.enc([]byte("bad key"))
+	if err != nil {
+		t.Fatalf("Unexpected failure when expected success: %v", err)
+	}
+	b, _, err = serialise.ToBytesMany(
+		[]any{
+			string("badID"),
+			b,
+		}, serialise.WithSerialisationApproach(serialise.NewMinDataApproachWithVersion(serialise.V1)))
+	if err != nil {
+		t.Fatalf("Unexpected failure when expected success: %v", err)
+	}
+
+	// Unknown ID error should be raised
+	b, err = p.Decrypt(context.TODO(), b)
+	if err == nil {
+		t.Fatal("Unexpected success when expected error")
+	}
+	if !errors.Is(err, errID) {
+		t.Fatalf("Unexpected error: expected: %v, got: %v", errID, err)
+	}
+	if b != nil {
+		t.Fatal("Unexpected instance returned when expected nil")
 	}
 }
